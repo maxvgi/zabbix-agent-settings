e217ac726fedf1935144679b76baadf1270927e3
Maxim Leontyev <leontyevmax@gmail.com>
2017-08-02 11:04:02 +0300

    Добавлен SMART monitoring

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,5 @@
 apt update
-apt install net-tools
+apt install net-tools smartmontools
 
-echo View notes in nginx.conf, fpm.conf and mysql.conf
+echo View notes in nginx.conf, fpm.conf, mysql.conf and smartctl.conf.
+echo Smartctl is only needed on physical servers.

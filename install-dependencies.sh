008fed004563412647ce8d4c6434d62e070f336a
Maxim Leontyev <leontyevmax@gmail.com>
2020-01-31 10:45:57 +0400

    Added mongodb monitoring

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,9 @@
 apt update
 apt install net-tools smartmontools sysstat
 
+#apt install python-pip
+#pip install pymongo
+
 echo ''
 echo View notes in nginx.conf, fpm.conf, mysql.conf and smartctl.conf.
 echo Smartctl is only needed on physical servers.
